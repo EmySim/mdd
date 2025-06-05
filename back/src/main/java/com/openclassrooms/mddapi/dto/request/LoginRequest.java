00340fa5f6cda89520859881fe47ab1f736d76e1
EmySim <145769796+EmySim@users.noreply.github.com>
2025-06-05 16:43:14 +0200

    feat: implement global exception handling and response DTOs for API

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/request/LoginRequest.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.mddapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * DTO pour les requêtes de connexion utilisateur.
+ *
+ * Contient les informations nécessaires pour l'authentification :
+ * - Email de l'utilisateur (identifiant unique)
+ * - Mot de passe en clair (sera vérifié côté serveur)
+ *
+ * Validation automatique via Bean Validation.
+ *
+ * @author Équipe MDD
+ * @version 1.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    /**
+     * Adresse email de l'utilisateur.
+     * Utilisée comme identifiant unique pour la connexion.
+     */
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
+    private String email;
+
+    /**
+     * Mot de passe de l'utilisateur en clair.
+     * Sera validé contre le hash stocké en base de données.
+     */
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    private String password;
+}
