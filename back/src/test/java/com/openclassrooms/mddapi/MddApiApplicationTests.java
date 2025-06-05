f0db626cc428c87f802cff36bf9c36da6d004bd1
EmySim <145769796+EmySim@users.noreply.github.com>
2025-06-05 13:52:41 +0200

    feat: add database configuration and initial SQL scripts for MDD application

--- back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
+++ back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.openclassrooms.mddapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Active le profil de test
 class MddApiApplicationTests {
-
 	@Test
 	void contextLoads() {
+		// Test que le contexte Spring se charge correctement
 	}
-
 }
